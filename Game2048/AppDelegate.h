e554031083348d2e0cc062e6228f15ab0313917b
lanou3g <lanou3g@lanou3gdeMac-mini.local>
2015-08-16 12:15:40 +0800

    Initial Commit

---
+++ Game2048/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Game2048
+//
+//  Created by lanou3g on 15/8/16.
+//  Copyright (c) 2015年 xhf06. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Game2048Tests/Game2048Tests.m
@@ -0,0 +1,34 @@
+//
+//  Game2048Tests.m
+//  Game2048Tests
+//
+//  Created by lanou3g on 15/8/16.
+//  Copyright (c) 2015年 xhf06. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Game2048Tests : XCTestCase
+
+@end
+
+@implementation Game2048Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
